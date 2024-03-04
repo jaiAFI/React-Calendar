1cddd710335d33eaea510f830d2a1e2f4acc97dd
jaiAFI <jai.afidigital@gmail.com>
2024-03-04 17:03:20 +0530

    implemented funtionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const dt = new Date();
+  const date = dt.getDate();
+  const [month, setMonth] = useState(dt.getMonth())
+  const year = dt.getFullYear();
+  const numberOfDaysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDayOfMonthIndex = new Date(year, month, 1).getDay();
+
 
   const daysArray = [];
-  for (let i = 1; i <= 30; i++) {
-    daysArray.push(i);
+  for (let i = 1; i <= numberOfDaysInMonth + firstDayOfMonthIndex; i++) {
+    if (i > firstDayOfMonthIndex) {
+      daysArray.push(i - firstDayOfMonthIndex);
+    } else {
+      daysArray.push("");
+    }
+  }
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const prevHandler = () => {
+    setMonth((prev)=> (prev-1))
+  }
+
+  const nextHandler = () => {
+    setMonth((prev)=> (prev+1))
   }
 
   return (
@@ -13,18 +49,20 @@ function App() {
       <div className="wrapper">
         <div className="container">
           <div className="header-container">
-            <div className="prev-button">Prev</div>
-            <div className="month-name">{`March, 2024`}</div>
-            <div className="next-button">Next</div>
+            <div className="button" onClick={prevHandler}>Prev</div>
+            <div className="month-name">{`${months[month]}, ${year}`}</div>
+            <div className="button" onClick={nextHandler}>Next</div>
           </div>
           <div className="daynames-container">
-            {days.map((day) => (
-              <div>{day}</div>
+            {days.map((day, index) => (
+              <div key={index}>{day}</div>
             ))}
           </div>
           <div className="days-container">
             {daysArray.map((number, index) => (
-              <div className="day-container" key={index}>{number}</div>
+              number!=="" ? <div className={`day-container ${(number === date) && (month==dt.getMonth()) ? "today" : ""}`} key={index}>
+                {number}
+              </div> : <div key={index}></div>
             ))}
           </div>
         </div>
